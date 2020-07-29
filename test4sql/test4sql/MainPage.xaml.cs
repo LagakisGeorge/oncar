a9b352e650da4069a15157b99e5c0c227b46e097
User <glagakis@gmail.com>
2020-07-29 22:40:19 +0300

    μικροαλλαγες

--- test4sql/test4sql/MainPage.xaml.cs
+++ test4sql/test4sql/MainPage.xaml.cs
@@ -22,6 +22,9 @@ namespace test4sql
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(true)]
+
+
+   
     public partial class MainPage : ContentPage
     {
 
